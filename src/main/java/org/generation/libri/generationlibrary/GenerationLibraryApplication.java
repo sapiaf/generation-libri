a6233da0d236a03e87cd45dfe153efdd7ab4aeb4
Francesco Sapia <123062954+sapiaf@users.noreply.github.com>
2023-10-04 11:39:40 +0200

    Project setup

---
+++ src/main/java/org/generation/libri/generationlibrary/GenerationLibraryApplication.java
@@ -0,0 +1,13 @@
+package org.generation.libri.generationlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerationLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerationLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/generation/libri/generationlibrary/GenerationLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.libri.generationlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerationLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
