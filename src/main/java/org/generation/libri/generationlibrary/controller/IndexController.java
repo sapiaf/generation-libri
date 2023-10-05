0c7ae38f5bc2dac1241472f414440f25e3aac25e
renat <guitarra123primera@gmail.com>
2023-10-05 11:46:04 +0200

    index controller finished.

--- src/main/java/org/generation/libri/generationlibrary/controller/IndexController.java
+++ src/main/java/org/generation/libri/generationlibrary/controller/IndexController.java
@@ -1,11 +1,16 @@
 package org.generation.libri.generationlibrary.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /* Homepage - user */
 @Controller
-@RequestMapping("/")
+@RequestMapping("/home")
 public class IndexController {
 
+    @GetMapping
+    private String index(){
+        return "/index";
+    }
 }
