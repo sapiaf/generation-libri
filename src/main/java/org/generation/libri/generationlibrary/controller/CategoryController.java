779943e6232bb8c0ec71b4df36728e2c71b90903
renat <guitarra123primera@gmail.com>
2023-10-05 10:41:23 +0200

    Dependencies disabled

--- src/main/java/org/generation/libri/generationlibrary/controller/CategoryController.java
+++ src/main/java/org/generation/libri/generationlibrary/controller/CategoryController.java
@@ -1,8 +1,11 @@
 package org.generation.libri.generationlibrary.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /* Crud categorie lato amministrazione*/
 @Controller
 public class CategoryController {
+
+    @GetMapping()
 }
