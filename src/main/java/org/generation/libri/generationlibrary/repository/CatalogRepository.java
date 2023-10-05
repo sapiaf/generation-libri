aad79efb883c090d364f2ccf2b0f7537aa09c733
juliekaam <nestlyguiabu@gmail.com>
2023-10-05 14:31:42 +0200

    catalogController done

--- src/main/java/org/generation/libri/generationlibrary/repository/CatalogRepository.java
+++ src/main/java/org/generation/libri/generationlibrary/repository/CatalogRepository.java
@@ -3,5 +3,5 @@ package org.generation.libri.generationlibrary.repository;
 import org.generation.libri.generationlibrary.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category,Integer> {
+public interface CatalogRepository extends JpaRepository<Category,Integer> {
 }
