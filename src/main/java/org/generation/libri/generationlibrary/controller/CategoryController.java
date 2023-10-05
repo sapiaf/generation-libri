fc46755175b5c93f02b4e6acee2b71c014ab1f4b
renat <guitarra123primera@gmail.com>
2023-10-05 14:32:42 +0200

    Fixed category ocntroller

--- src/main/java/org/generation/libri/generationlibrary/controller/CategoryController.java
+++ src/main/java/org/generation/libri/generationlibrary/controller/CategoryController.java
@@ -5,14 +5,12 @@ import org.generation.libri.generationlibrary.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /* Crud categorie lato amministrazione*/
-@Controller("/admin/categories")
+@Controller
+@RequestMapping("/admin/categories")
 public class CategoryController {
     @Autowired
     CategoryRepository categoryRepository;
@@ -24,13 +22,18 @@ public class CategoryController {
         model.addAttribute("categoryObj", new Category());
         return "/admin/categories/list";
     }
+
+    //Metodo che crea una nuova categoria
     @PostMapping("/create")
     public String doCreate(@ModelAttribute("categoryObj") Category categoryform, RedirectAttributes redirectAttributes) {
         categoryRepository.save(categoryform);
         redirectAttributes.addFlashAttribute("message", "Categpry succesfully added");
-
         return "redirect:/admin/categories";
     }
+    //Metodo per l'update
+
+
+
     //Metodo che cancella una categoria
     @PostMapping("/delete/{catId}")
     public String delete(@PathVariable("catId") int id){
