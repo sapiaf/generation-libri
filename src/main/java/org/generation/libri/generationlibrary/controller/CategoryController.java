7c41250b26d7a60f169dcc2462f06260cdb13391
renat <guitarra123primera@gmail.com>
2023-10-05 15:15:42 +0200

    Category Controlled fixed and finished

--- src/main/java/org/generation/libri/generationlibrary/controller/CategoryController.java
+++ src/main/java/org/generation/libri/generationlibrary/controller/CategoryController.java
@@ -1,12 +1,18 @@
 package org.generation.libri.generationlibrary.controller;
 
+import jakarta.validation.Valid;
 import org.generation.libri.generationlibrary.model.Category;
 import org.generation.libri.generationlibrary.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
 
 /* Crud categorie lato amministrazione*/
 @Controller
@@ -27,15 +33,29 @@ public class CategoryController {
     @PostMapping("/create")
     public String doCreate(@ModelAttribute("categoryObj") Category categoryform, RedirectAttributes redirectAttributes) {
         categoryRepository.save(categoryform);
-        redirectAttributes.addFlashAttribute("message", "Categpry succesfully added");
+        redirectAttributes.addFlashAttribute("message", "Category succesfully added");
         return "redirect:/admin/categories";
     }
 
     //Metodo per l'update
-    @GetMapping("/update")
-    public String update() {
+    @GetMapping("/update/{catId}")
+    public String update(@PathVariable Integer id, Model model) {
+        Optional<Category> result = categoryRepository.findById(id);
+        if (result.isPresent()) {
+            model.addAttribute("category", result.get());
+            return "/admin/categories/update";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category with id " + id + " not found");
+        }
+    }
 
-        return "";
+    @PostMapping("/update/{catId}")
+    public String doUpdate(@PathVariable Integer id, @Valid @ModelAttribute("category") Category categoryForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "/admin/categories/update";
+        }
+        categoryRepository.save(categoryForm);
+        return "redirect:/admin/categories/list";
     }
 
 
@@ -45,4 +65,6 @@ public class CategoryController {
         categoryRepository.deleteById(id);
         return "redirect:/admin/categories";
     }
+
+    //SEARCH
 }
