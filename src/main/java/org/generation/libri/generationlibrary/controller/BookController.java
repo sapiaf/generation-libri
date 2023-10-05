3276362d49f6e00185529fa0d4b4ae0cdfeded66
renat <guitarra123primera@gmail.com>
2023-10-05 10:37:55 +0200

    Dependencies disabled

--- src/main/java/org/generation/libri/generationlibrary/controller/BookController.java
+++ src/main/java/org/generation/libri/generationlibrary/controller/BookController.java
@@ -3,9 +3,21 @@ package org.generation.libri.generationlibrary.controller;
 /*tutta la crud dei book - ambiente amministrazione*/
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
 public class BookController {
+
+    //CREATE
+    @GetMapping("/create")
+    private String create (){
+        return "/admin/books/create";
+    }
+
+    //UPDATE
+
+    //DELETE
+
 }
--- src/main/java/org/generation/libri/generationlibrary/controller/IndexController.java
+++ src/main/java/org/generation/libri/generationlibrary/controller/IndexController.java
@@ -7,4 +7,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class IndexController {
+
 }
