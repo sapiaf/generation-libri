c69142ea0af36e8283843793627266b78984ae55
Oblv49 <noprivacyworld@gmail.com>
2023-10-06 04:12:38 +0200

    fixed update book + templates

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,8 @@
-INSERT INTO book (name, url_photo, description, copies, price, sold_copies) VALUES('Harry Potter and the Chamber of Secrets', 'http://example.com/images/harrypotter.jpg', 'The second installment in the Harry Potter series.', 1000000, 19.99, 4);
-INSERT INTO book (name, url_photo, description, copies, price, sold_copies) VALUES('Dune', 'http://example.com/images/dune.jpg', 'A science fiction novel set in a desert world.', 750000, 24.99, 4);
-INSERT INTO book (name, url_photo, description, copies, price, sold_copies) VALUES('The Great Gatsby', 'http://example.com/images/gatsby.jpg', 'A classic novel set in the Roaring Twenties.', 500000, 15.99, 3);
-INSERT INTO book (name, url_photo, description, copies, price, sold_copies) VALUES('1984', 'http://example.com/images/1984.jpg', 'George Orwell''s dystopian masterpiece.', 800000, 20.00, 3);
-INSERT INTO book (name, url_photo, description, copies, price, sold_copies) VALUES('To Kill a Mockingbird', 'http://example.com/images/mockingbird.jpg', 'A novel of racism and injustice.', 4, 18.50, 2);
+INSERT INTO book (name, url_photo, description, copies, price, sold_copies) VALUES('Harry Potter and the Chamber of Secrets', 'https://m.media-amazon.com/images/I/91OINeHnJGL._AC_UF1000,1000_QL80_.jpg', 'The second installment in the Harry Potter series.', 1000000, 19.99, 4);
+INSERT INTO book (name, url_photo, description, copies, price, sold_copies) VALUES('Dune', 'https://m.media-amazon.com/images/I/71dwfOuYLCL._AC_UF1000,1000_QL80_.jpg', 'A science fiction novel set in a desert world.', 750000, 24.99, 4);
+INSERT INTO book (name, url_photo, description, copies, price, sold_copies) VALUES('The Great Gatsby', 'https://m.media-amazon.com/images/I/71FTb9X6wsL._AC_UF1000,1000_QL80_.jpg', 'A classic novel set in the Roaring Twenties.', 500000, 15.99, 3);
+INSERT INTO book (name, url_photo, description, copies, price, sold_copies) VALUES('1984', 'https://www.einaudi.it/content/uploads/2021/01/978880624818HIG.JPG', 'George Orwell''s dystopian masterpiece.', 800000, 20.00, 3);
+INSERT INTO book (name, url_photo, description, copies, price, sold_copies) VALUES('To Kill a Mockingbird', 'https://m.media-amazon.com/images/I/81NeMmw4RQL._SL1500_.jpg', 'A novel of racism and injustice.', 4, 18.50, 2);
 INSERT INTO category (name) VALUES('fiction');
 INSERT INTO category (name) VALUES('teenagers');
 INSERT INTO category (name) VALUES('kids');
