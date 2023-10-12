d8c1199c69e9e0b67c09027dfd63d845120355e4
Francesco Sapia <123062954+sapiaf@users.noreply.github.com>
2023-10-12 09:32:19 +0200

    mega menu navbar, added queries for bestsellers, recent books, home book sections and admin legend and footer

--- src/main/java/org/generation/libri/generationlibrary/controller/IndexController.java
+++ src/main/java/org/generation/libri/generationlibrary/controller/IndexController.java
@@ -2,12 +2,15 @@ package org.generation.libri.generationlibrary.controller;
 
 import org.generation.libri.generationlibrary.model.Book;
 import org.generation.libri.generationlibrary.repository.BookRepository;
+import org.generation.libri.generationlibrary.repository.PurchaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,22 +20,28 @@ import java.util.List;
 public class IndexController {
     @Autowired
     BookRepository bookRepository;
+    @Autowired
+    PurchaseRepository purchaseRepository;
 
 
     @GetMapping
     public String index(Model model) {
-        List<Book> bookList = bookRepository.findAll();
-        List<Book> bookSelected = new ArrayList<>();
-        for (Book book : bookList) {
-            if (!book.isDeleteTrue()) {
-                bookSelected.add(book);
-            }
-        }
-        model.addAttribute("books", bookSelected);
+        List<Book> bookList = filterDeletedBooks(bookRepository.findAllByOrderByDateOfPublishingDesc());
+        List<Book> narrativaBooks = filterDeletedBooks(bookRepository.findByCategories_Id(1));
+        List<Book> fantasyBooks = filterDeletedBooks(bookRepository.findByCategories_Id(5));
+        List<Book> classiciBooks = filterDeletedBooks(bookRepository.findByCategories_Id(2));
+        LocalDateTime endDate = LocalDateTime.now();
+        LocalDateTime startDate = endDate.minusMonths(1);
+        List<Book> topSellingBooks = filterDeletedBooks(purchaseRepository.findTopSellingBooksInDateRange(startDate, endDate));
+
+        model.addAttribute("narrativaBooks", narrativaBooks);
+        model.addAttribute("fantasyBooks", fantasyBooks);
+        model.addAttribute("classiciBooks", classiciBooks);
+        model.addAttribute("topSellingBooks", topSellingBooks);
+        model.addAttribute("books", bookList);
+
         return "/index";
     }
-
-
     @GetMapping("/aiuto-faq")
     public String qAndA() {
         return "/user/info/qa";
@@ -51,5 +60,15 @@ public class IndexController {
     @GetMapping("/spedizioni")
     public String spedizioni() {
         return "/user/info/shipping";
+    }
+
+    private List<Book> filterDeletedBooks(List<Book> books) {
+        List<Book> filteredBooks = new ArrayList<>();
+        for (Book book : books) {
+            if (!book.isDeleteTrue()) {
+                filteredBooks.add(book);
+            }
+        }
+        return filteredBooks;
     }
 }
--- src/main/java/org/generation/libri/generationlibrary/repository/PurchaseRepository.java
+++ src/main/java/org/generation/libri/generationlibrary/repository/PurchaseRepository.java
@@ -1,7 +1,16 @@
 package org.generation.libri.generationlibrary.repository;
 
+import org.generation.libri.generationlibrary.model.Book;
 import org.generation.libri.generationlibrary.model.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface PurchaseRepository extends JpaRepository<Purchase, Integer> {
+    @Query("SELECT p.book, SUM(p.purchaseQuantity) as totalSold FROM Purchase p WHERE p.dateOfPurchase BETWEEN :startDate AND :endDate GROUP BY p.book ORDER BY totalSold DESC")
+    List<Book> findTopSellingBooksInDateRange(LocalDateTime startDate, LocalDateTime endDate);
 }
