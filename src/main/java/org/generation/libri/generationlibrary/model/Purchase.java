f2d1c2c3a9045e256c3aeddab968696bb1fc8bc9
francesca <francipeco36@gmail.com>
2023-10-13 20:19:30 +0200

    adding user info in pruchase + purchase detail updated

--- src/main/java/org/generation/libri/generationlibrary/model/Purchase.java
+++ src/main/java/org/generation/libri/generationlibrary/model/Purchase.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
-public class    Purchase {
+public class Purchase {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -16,15 +16,34 @@ public class    Purchase {
     @NotNull
     @Min(1)
     private int purchaseQuantity;
+    private String userName;
+    private String userSurname;
+    private String userEmail;
+    private String userAddress;
+    private String userCountry;
+    private String userCity;
+    private String userMunicipality;
+    private String userZipCode;
+    private String userPaymentMode;
 
     @ManyToOne
     private Book book;
 
     //constructor
-    public Purchase(int id, LocalDateTime dateOfPurchase, int purchaseQuantity, Book book) {
+
+    public Purchase(int id, LocalDateTime dateOfPurchase, @NotNull int purchaseQuantity, String userName, String userSurname, String userEmail, String userAddress, String userCountry, String userCity, String userMunicipality, String userZipCode, String userPaymentMode, Book book) {
         this.id = id;
         this.dateOfPurchase = dateOfPurchase;
         this.purchaseQuantity = purchaseQuantity;
+        this.userName = userName;
+        this.userSurname = userSurname;
+        this.userEmail = userEmail;
+        this.userAddress = userAddress;
+        this.userCountry = userCountry;
+        this.userCity = userCity;
+        this.userMunicipality = userMunicipality;
+        this.userZipCode = userZipCode;
+        this.userPaymentMode = userPaymentMode;
         this.book = book;
     }
 
@@ -65,6 +84,77 @@ public class    Purchase {
         this.book = book;
     }
 
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserSurname() {
+        return userSurname;
+    }
+
+    public void setUserSurname(String userSurname) {
+        this.userSurname = userSurname;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public String getUserCountry() {
+        return userCountry;
+    }
+
+    public void setUserCountry(String userCountry) {
+        this.userCountry = userCountry;
+    }
+
+    public String getUserCity() {
+        return userCity;
+    }
+
+    public void setUserCity(String userCity) {
+        this.userCity = userCity;
+    }
+
+    public String getUserMunicipality() {
+        return userMunicipality;
+    }
+
+    public void setUserMunicipality(String userMunicipality) {
+        this.userMunicipality = userMunicipality;
+    }
+
+    public String getUserZipCode() {
+        return userZipCode;
+    }
+
+    public void setUserZipCode(String userZipCode) {
+        this.userZipCode = userZipCode;
+    }
+
+    public String getUserPaymentMode() {
+        return userPaymentMode;
+    }
+
+    public void setUserPaymentMode(String userPaymentMode) {
+        this.userPaymentMode = userPaymentMode;
+    }
 
     public BigDecimal getTotalPrice() {
         BigDecimal totalPrice = book.getPrice().multiply(BigDecimal.valueOf(purchaseQuantity));
